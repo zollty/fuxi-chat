2a4020889af6497768706f23545bf01a025ed9f1
zollty <zollty@163.com>
2024-03-08 17:59:14 +0800

    update

--- llm_chat/config.py
+++ llm_chat/config.py
@@ -13,8 +13,12 @@ from typing import (
 
 
 def fschat_openai_api_cfg():
+    # if host == "0.0.0.0":
+    #     host = "127.0.0.1"
+    # port = FSCHAT_OPENAI_API["port"]
+    # return f"http://{host}:{port}/v1"
     # address, api_key
-    return "http://localhost:20000", "EMPTY"
+    return "http://localhost:20000/v1", "EMPTY"
 
 
 def fschat_controller_address():
