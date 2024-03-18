b0c59f1a241f0e5d1dcce1a618b94ac4a8ed42e8
zollty <zollty@163.com>
2024-03-18 17:00:21 +0800

    update

--- retrieval_tests/redis_search_test.py
+++ retrieval_tests/redis_search_test.py
@@ -76,8 +76,8 @@ if __name__ == '__main__':
     # insert_doc(client, docs, kb_name, use_id="key")
 
     sentences = [
-        "白蛇娘子*",
-        "扬州园*",
+        "*白蛇娘子*",
+        "*扬州园*",
         "美国",
         "照壁",
         "博纳",
