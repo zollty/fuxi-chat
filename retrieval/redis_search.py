997210c4a35be9479861659aabdf2e6074ae1ae2
zollty <zollty@163.com>
2024-03-18 16:33:08 +0800

    update

--- retrieval/redis_search.py
+++ retrieval/redis_search.py
@@ -102,7 +102,7 @@ def insert_doc(client: redis.Redis, docs: List[DocSchema], kb_name: str, use_id:
 def retrieve_docs(client: redis.Redis, query: str, kb_name: str):
     s = get_short_url(kb_name)
     name = "doc:" + s
-    query = Query(f"@doc:{query}")
+    query = Query(query)#Query(f"@doc:{query}")
     res = client.ft(name).search(query, {"language": "chinese"}).docs
     print(res)
     return res
