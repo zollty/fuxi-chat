b58cac234207ea068bde90f6259b6eb59977b25e
zollty <zollty@163.com>
2024-03-18 13:48:15 +0800

    update

---
+++ retrieval/redis_search.py
@@ -0,0 +1,143 @@
+import time
+from typing import Any, List, Optional, Dict
+
+import hashlib
+
+
+def get_short_url(url):
+    md5 = hashlib.md5(url.encode('utf-8')).hexdigest()
+    # 将32位的md5哈希值转化为10进制数
+    num = int(md5, 16)
+    # 将10进制数转化为62进制数
+    base = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    short_url = ''
+    while num > 0:
+        short_url += base[num % 62]
+        num //= 62
+    # 短链接的长度为6位
+    return short_url[:6]
+
+
+from redisvl.index import SearchIndex
+from redisvl.utils.vectorize import HFTextVectorizer
+from redisvl.query import VectorQuery
+
+import json
+import time
+
+import numpy as np
+import redis
+from redis.commands.search.field import (
+    NumericField,
+    TagField,
+    TextField,
+    VectorField,
+)
+from redis.commands.search.indexDefinition import IndexDefinition, IndexType
+from redis.commands.search.query import Query
+
+DEFAULT_EMBED_PATH = "/ai/models/bce-embedding-base_v1"
+DEFAULT_EMBED_DIMS = 768
+REDIS_URL = "redis://127.0.0.1:6389"
+EMBED_BATCH_SIZE = 16
+DEFAULT_VEC_NUM = 20
+
+
+class DocSchema:
+    nid: int = 0
+    key: str = ""
+    src: str = ""
+    doc: str
+
+    def __init__(self, doc: str, src: str = "", key: str = "", nid: int = 0):
+        self.nid = nid
+        self.key = key
+        self.src = src
+        self.doc = doc
+
+
+def create_schema(client, kb_name: str):
+    s = get_short_url(kb_name)
+    prefix = "r:" + s
+    name = "doc:" + s
+
+    schema = (
+        TextField("doc"),
+        TagField("key", as_name="type"),
+        TagField("src", as_name="type"),
+    )
+
+    definition = IndexDefinition(prefix=[prefix], index_type=IndexType.JSON)
+    res = client.ft(name).create_index(
+        fields=schema, definition=definition
+    )
+    print(res)
+    # >>> 'OK'
+
+    return True
+
+
+def create_and_run_index(kb_name: str, dims: int = DEFAULT_EMBED_DIMS, redis_url: str = REDIS_URL):
+    schema = create_schema(kb_name, dims)
+    index = SearchIndex.from_dict(schema)
+    # connect to local redis instance
+    index.connect(redis_url)
+
+    # create the index (no data yet)
+    index.create(overwrite=True)
+    return index
+
+
+def insert_doc(docs: List[DocSchema], kb_name: str, use_id: str = None, redis_url: str = REDIS_URL,
+               embedd_path: str = DEFAULT_EMBED_PATH):
+    sentences = [t.doc for t in docs]
+
+    # Embedding a single text
+    vectorizer = HFTextVectorizer(model=embedd_path)
+    # Embedding a batch of texts
+    embeddings = vectorizer.embed_many(sentences, batch_size=EMBED_BATCH_SIZE, as_buffer=True)
+    dims = len(embeddings[0])
+
+    schema = create_schema(kb_name, dims)
+    index = SearchIndex.from_dict(schema)
+    # connect to local redis instance
+    index.connect(redis_url)  # "redis://127.0.0.1:6389"
+
+    data = [{"doc": t.doc,
+             "key": t.key,
+             "nid": t.nid,
+             "src": t.src,
+             "embed": v}
+            for t, v in zip(docs, embeddings)]
+
+    # load装载数据
+    if use_id:
+        index.load(data, id_field=use_id)
+    else:
+        index.load(data)
+
+
+def retrieve_docs(client, query: str, kb_name: str):
+    s = get_short_url(kb_name)
+    name = "doc:" + s
+    query = Query(f"@doc:({query})")
+    res = client.ft(name).search(query).docs
+    print(res)
+    return res
+
+
+if __name__ == '__main__':
+
+    client = redis.Redis(host="127.0.0.1", port=6389, decode_responses=True)
+
+    res = client.ping()
+    print(res)
+    # >>> True
+
+    kb_name = "yby"
+    create_schema(client, kb_name)
+
+    results = retrieve_docs(client, "江苏扬州园", kb_name)
+
+    for x in results:
+        print(x)
---
+++ retrieval/retrieval_design.md
@@ -0,0 +1,59 @@
+
+
+# 核心设计
+
+
+### 1、Redis向量库
+
+结构如下：
+
+```python
+def create_schema(kb_name: str, dims: int = DEFAULT_EMBED_DIMS):
+    s = get_short_url(kb_name)
+    prefix = "r:" + s
+    name = "idx:" + s
+
+    schema = {
+        "index": {
+            "name": name,
+            "prefix": prefix,
+        },
+        "fields": [
+            {"name": "nid", "type": "numeric"},  # id，和key二选一即可
+            {"name": "key", "type": "tag"},  # 唯一key，和nid二选一即可
+            {"name": "src", "type": "tag"},  # 来源，例如文件名
+            {"name": "doc", "type": "text"},
+            {
+                "name": "embed",
+                "type": "vector",
+                "attrs": {
+                    "dims": dims,
+                    "distance_metric": "cosine",
+                    "algorithm": "flat",
+                    "datatype": "float32"
+                }
+            }
+        ]
+    }
+
+    return schema
+```
+这个设计是通用的、灵活的：
+
+- 其中，nid和key对应的都是文档在某个src下的唯一ID/KEY，可以根据不同场景二选一。而src，是一个标签，可以表示文档的上级来源（例如某某文件，src就是文件名）。
+
+- nid是数字类型，是由数据库ID或者唯一ID生成器，来生成的。key也类似，但为string类型，例如文档的MD5值或者shortURL、gitcommitID算法得到的值。
+
+- doc字段，存储文档原文内容，也可以留空（不保存原文，原文在其他数据库中保存，那么这里可以留空或者存一个MD5/SHA值也行）。
+
+
+### 2、向量值（embed字段，存储向量数组）
+为了提高向量查询精度，embedding文本长度默认设置短一点，例如100~250，然后通过前后文映射成一个长文本段落（例如映射成单段3000字）。
+
+这就需要，文档分段存储的ID，与向量库的向量nid/key做一个映射。这个可以借助src字段，存储文档分段的ID即可解决。
+
+### 3、全文检索及文本存储
+
+
+
+
