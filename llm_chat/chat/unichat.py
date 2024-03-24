abb0d2bad07172c7c29abded6e72d7cb11bde566
zollty <zollty@163.com>
2024-03-24 20:01:38 +0800

    update

--- llm_chat/chat/unichat.py
+++ llm_chat/chat/unichat.py
@@ -5,6 +5,12 @@ from typing import Dict, List, Optional, Union, AsyncGenerator
 from jian.llm_chat.config import default_model, default_temperature
 from jian.llm_chat.chat.worker_direct_chat import check_requests, ChatCompletionRequest, chat_iter, chat_iter_given_txt
 
+help_doc = """**帮助文档（cmd指令）**
+（输入--help查看帮助）
+1、--url [url] [提问] （获取url网页内容并提问，限8千字）
+2、--search [搜索提问] （联网搜索再回答）
+3、--kb [知识库名，例如：数地、园博园] （搜索知识库再回答）
+"""
 
 
 async def unichat(request: ChatCompletionRequest):
@@ -25,8 +31,9 @@ async def unichat(request: ChatCompletionRequest):
             print(message)
             if message.get("role") == "user":
                 content = message.get("content")
-                if content is not None and content=="help":
-                    ret_text = "帮助文档\n\n1、输入cmd查看"
+                if content is not None and content.strip() == "--help":
+                    ret_text = help_doc
+
                     async def coro_chat_iter1() -> AsyncGenerator[str, None]:
                         async for item in chat_iter_given_txt(ret_text, stream=stream, model_name=model_name):
                             yield json.dumps(item.to_openai_dict(), ensure_ascii=False)
