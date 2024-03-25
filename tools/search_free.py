c05ad15985deecceed31771ab59b8f8c60d0d426
zollty <zollty@163.com>
2024-03-25 13:47:03 +0800

    update

--- tools/search_free.py
+++ tools/search_free.py
@@ -1,5 +1,4 @@
 import asyncio
-from fastapi.concurrency import run_in_threadpool
 from asyncache import cached
 from cachetools import TTLCache
 from jian.tools.webpage_loader import load_webpage
@@ -20,6 +19,7 @@ async def do_search_engine(text, result_len: int = SEARCH_ENGINE_TOP_K):
     for row in results.results():
         info += row.get("title") + "\n" + row.get("text")
         host = row.get('host')
+        # 过滤掉无效网页
         if ("youtube.com" in host
                 or "weibo.com" in host
                 or "google.com" in host
@@ -37,8 +37,6 @@ async def do_search_engine(text, result_len: int = SEARCH_ENGINE_TOP_K):
             continue
         if row.get('link').endswith('.pdf'):
             continue
-        # context = await load_webpage(row.get('link'), 30000)
-        # ctx.append(context)
         links.append(row.get('link'))
         if len(links) >= result_len:
             break
@@ -51,25 +49,9 @@ async def do_search_engine(text, result_len: int = SEARCH_ENGINE_TOP_K):
         all_tasks = await asyncio.gather(*chat_completions)
     except Exception as e:
         print(e)
-        return ""
+        return info
 
     for i, content in enumerate(all_tasks):
         ctx.append(content)
 
     return info + "\n" + "\n".join(ctx)
-
-
-# @cached(TTLCache(100, 600))
-async def search_engine_iter(query: str):
-    # results = await run_in_threadpool(do_search_engine, query)
-    # return results
-    return ""
-
-
-def search_internet(query: str):
-    return asyncio.run(search_engine_iter(query))
-
-
-if __name__ == "__main__":
-    result = search_internet("今天星期几")
-    print("答案:", result)
