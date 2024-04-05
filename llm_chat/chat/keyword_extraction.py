537f3428b227c7315252679d3e01ec826f025ab8
zollty <zollty@163.com>
2024-04-05 20:37:31 +0800

    update

--- llm_chat/chat/keyword_extraction.py
+++ llm_chat/chat/keyword_extraction.py
@@ -1,14 +1,15 @@
 from typing import AsyncIterable, Optional
+from fastapi import Body
 from fastapi.responses import StreamingResponse, JSONResponse
 from jian.llm_chat.chat.utils import format_jinja2_prompt_tmpl
 from jian.llm_chat.chat.worker_direct_chat import chat_iter, ChatCompletionRequest
 from jian.llm_chat.config import default_model
 
 
-async def keyword_extraction(sentence: str,
-                             model_name: str = None,
-                             max_tokens: Optional[int] = 2000,
-                             temperature: Optional[float] = 0.1,
+async def keyword_extraction(sentence: str = Body(..., description="输入内容"),
+                             model_name: str = Body(None, description="模型"),
+                             max_tokens: Optional[int] = Body(2000, description="设置最大token"),
+                             temperature: Optional[float] = Body(0.1, description="设置temperature"),
                              ):
     if not model_name:
         model_name = default_model()
