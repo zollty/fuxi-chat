5f8f0f7cbe9cbd3095b018d81256bacf1655c354
zollty <zollty@163.com>
2024-04-13 22:22:43 +0800

    update

--- llm_chat/chat/yby_chat2.py
+++ llm_chat/chat/yby_chat2.py
@@ -2,15 +2,16 @@ from fastapi import Body, File, Form, UploadFile
 from sse_starlette.sse import EventSourceResponse
 from langchain.chains import LLMChain
 from langchain.callbacks import AsyncIteratorCallbackHandler
-from typing import AsyncIterable, List, Optional
+from typing import AsyncIterable, List, Optional, Any, Callable
 import asyncio
 from langchain.prompts.chat import ChatPromptTemplate
 import json
 
 from langchain.document_loaders import TextLoader
 
-from llm_chat.chat.utils import History, get_ChatOpenAI, wrap_done
-from llm_chat.config import get_prompt_template, LONG_CONTEXT_MODEL, file_chat_default_temperature
+from jian.llm_chat.chat.utils import History
+from jian.llm_chat.config import get_prompt_template, file_chat_default_temperature, default_long_context_model
+from fuxi.utils.thread_helper import wrap_done
 
 # 读取原始文档
 # raw_documents_sanguo = TextLoader('/ai/apps/data/new/园博园参考资料.txt', encoding='utf-8').load()
@@ -22,7 +23,38 @@ raw_documents_sanguo = TextLoader('/ai/apps/data/园博园参考资料.txt', enc
 raw_documents_xiyou = TextLoader('/ai/apps/data/园博园介绍.txt', encoding='utf-8').load()
 raw_documents_fw = TextLoader('/ai/apps/data/园博园服务.txt', encoding='utf-8').load()
 yby_src = raw_documents_sanguo + raw_documents_xiyou + raw_documents_fw
-YBY_DEFAULT_LLM = LONG_CONTEXT_MODEL
+
+
+from langchain.chat_models import ChatOpenAI
+from jian.llm_chat.chat.minx_chat_openai import MinxChatOpenAI
+from jian.llm_chat import config
+def get_ChatOpenAI(
+        model_name: str,
+        temperature: float,
+        max_tokens: int = None,
+        streaming: bool = True,
+        callbacks: List[Callable] = [],
+        verbose: bool = True,
+        **kwargs: Any,
+) -> ChatOpenAI:
+    # 非Langchain原生支持的模型，走Fschat封装
+    if model_name == "openai-api":
+        model_name = config.default_openai_model()
+    ChatOpenAI._get_encoding_model = MinxChatOpenAI.get_encoding_model
+    api_base_url, api_key = config.openai_api_cfg()
+    model = ChatOpenAI(
+        streaming=streaming,
+        verbose=verbose,
+        callbacks=callbacks,
+        openai_api_key=api_key,
+        openai_api_base="http://127.0.0.1:30000/v1",
+        model_name=model_name,
+        temperature=temperature,
+        max_tokens=max_tokens,
+        openai_proxy=config.openai_proxy(),
+        **kwargs
+    )
+    return model
 
 
 async def yby_chat(query: str = Body(..., description="用户输入", examples=["你好"]),
@@ -31,7 +63,7 @@ async def yby_chat(query: str = Body(..., description="用户输入", examples=[
                                                  description="历史对话",
                                                  ),
                    stream: bool = Body(False, description="流式输出"),
-                   model_name: str = Body(YBY_DEFAULT_LLM, description="LLM 模型名称。"),
+                   model_name: str = Body(None, description="LLM 模型名称。"),
                    temperature: float = Body(file_chat_default_temperature(), description="LLM 采样温度", ge=0.0,
                                              le=1.0),
                    max_tokens: Optional[int] = Body(None, description="限制LLM生成Token数量，默认None代表模型最大值"),
@@ -50,6 +82,10 @@ async def yby_chat(query: str = Body(..., description="用户输入", examples=[
         callback = AsyncIteratorCallbackHandler()
         if isinstance(max_tokens, int) and max_tokens <= 0:
             max_tokens = None
+
+        if not model_name:
+            model_name = default_long_context_model()
+
 
         print(f"----------------------------------------------------get model {model_name}, max_tokens={max_tokens}")
         model = get_ChatOpenAI(
