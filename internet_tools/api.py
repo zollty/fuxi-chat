9baaf184e1f27a91dde837698699c9e9e19bbe04
zollty <zollty@163.com>
2024-06-02 15:50:30 +0800

    update

---
+++ internet_tools/api.py
@@ -0,0 +1,68 @@
+import sys
+import os
+
+# 获取当前脚本的绝对路径
+__current_script_path = os.path.abspath(__file__)
+# 将项目根目录添加到sys.path
+runtime_root_dir = os.path.dirname(os.path.dirname(__current_script_path))
+sys.path.append(runtime_root_dir)
+
+from fastapi import FastAPI, Body, Request, File, Form, UploadFile
+from starlette.responses import RedirectResponse
+from fuxi.utils.api_base import (BaseResponse, ListResponse)
+
+
+async def document():
+    return RedirectResponse(url="/docs")
+
+def mount_app_routes(app: FastAPI):
+    from jian.tools.search_free import do_search_engine
+
+    from jian.tools.langchain_utils import test_parse_url
+
+    def  search_engine(
+            query: str = Form("", description="查询语句"),
+            engine: str = Form("google", description="搜索引擎")
+    ) -> BaseResponse:
+        context = do_search_engine(query)
+        if context:
+            return BaseResponse(code=200, msg="搜索成功", data=context)
+        return BaseResponse(code=500, msg="搜索失败")
+
+    app.get("/",
+            response_model=BaseResponse,
+            summary="swagger 文档")(document)
+
+    app.post("/internet/search_engine",
+             tags=["Internet"],
+             summary="使用互联网搜索引擎（默认Google）"
+             )(search_engine)
+
+    app.post("/internet/test_parse_url",
+             tags=["Tools"],
+             summary="解析url并分段，返回分段文本内容"
+             )(test_parse_url)
+
+
+if __name__ == "__main__":
+    import argparse
+    from fuxi.utils.fastapi_tool import create_app, run_api
+
+    parser = argparse.ArgumentParser(prog='fenghou-ai',
+                                     description='About FenghouAI-internet_tools API')
+    parser.add_argument("--host", type=str, default="0.0.0.0")
+    parser.add_argument("--port", type=int, default=7962)
+    parser.add_argument("--ssl_keyfile", type=str)
+    parser.add_argument("--ssl_certfile", type=str)
+    # 初始化消息
+    args = parser.parse_args()
+    args_dict = vars(args)
+
+    app = create_app([mount_app_routes], version="1.0.0", title="FenghouAI internet_tools API Server")
+
+    run_api(app,
+            host=args.host,
+            port=args.port,
+            ssl_keyfile=args.ssl_keyfile,
+            ssl_certfile=args.ssl_certfile,
+            )
---
+++ internet_tools/hp_internet_tools_server.py
@@ -0,0 +1,81 @@
+import sys
+import os
+
+# 获取当前脚本的绝对路径
+__current_script_path = os.path.abspath(__file__)
+# 将项目根目录添加到sys.path
+runtime_root_dir = os.path.dirname(os.path.dirname(os.path.dirname(__current_script_path)))
+sys.path.append(runtime_root_dir)
+
+from dynaconf import Dynaconf
+
+def base_init_0(cfg: Dynaconf, log_level):
+    from fuxi.utils.fastapi_tool import run_api, create_app
+    from jian.internet_tools.api import mount_app_routes
+
+    host = cfg.get("agent.internet_tools_server.host")
+    port = cfg.get("agent.internet_tools_server.port")
+    if host == "localhost" or host == "127.0.0.1":
+        host = "0.0.0.0"
+
+    app = create_app([mount_app_routes], version="1.0.0", title="FenghouAI internet_tools API Server")
+
+    run_api(
+        app,
+        host=host,
+        port=port,
+        log_level=log_level,
+        ssl_keyfile=cfg.get("agent.internet_tools_server.ssl_keyfile", cfg.get("root.ssl_keyfile", None)),
+        ssl_certfile=cfg.get("agent.internet_tools_server.ssl_certfile", cfg.get("root.ssl_certfile", None)),
+    )
+
+
+def init_api_server():
+    import argparse
+    from fuxi.utils.runtime_conf import get_runtime_root_dir
+
+    print(get_runtime_root_dir())
+    cfg = Dynaconf(
+        envvar_prefix="JIAN",
+        root_path=get_runtime_root_dir(),
+        settings_files=['conf/llm_model.yml', 'conf/settings.yaml'],
+    )
+    import jian.common.base_config as bc
+    bc.cfg = cfg
+
+    log_level = cfg.get("agent.internet_tools_server.log_level", "info")
+    verbose = True if log_level == "debug" else False
+    host = cfg.get("agent.internet_tools_server.host", "0.0.0.0")
+    port = cfg.get("agent.internet_tools_server.port", 8111)
+
+    parser = argparse.ArgumentParser(prog='FenghouAI',
+                                     description='About FenghouAI-internet_tools API')
+    parser.add_argument("--host", type=str, default=host)
+    parser.add_argument("--port", type=int, default=port)
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        help="增加log信息",
+        dest="verbose",
+        type=bool,
+        default=verbose,
+    )
+    # 初始化消息
+    args = parser.parse_args()
+    host = args.host
+    port = args.port
+    if args.verbose:
+        log_level = "debug"
+        cfg["agent.internet_tools_server.log_level"] = "debug"
+
+    cfg["agent.internet_tools_server.host"] = host
+    cfg["agent.internet_tools_server.port"] = port
+
+    from fuxi.utils.fastapi_tool import set_httpx_config
+    set_httpx_config()
+
+    base_init_0(cfg, log_level)
+
+
+if __name__ == "__main__":
+    init_api_server()
