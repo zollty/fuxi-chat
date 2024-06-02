fa948a62107c7ddbc4a15be6da7f83edf3d0cb98
zollty <zollty@163.com>
2024-06-02 20:53:52 +0800

    update

--- tools/search_free.py
+++ tools/search_free.py
@@ -11,6 +11,9 @@ async def do_search_engine(text, result_len: int = SEARCH_ENGINE_TOP_K):
     from search_engines import Google
 
     engine = Google()
+    import urllib.request
+    proxies = urllib.request.getproxies
+    print(f"----------------------proxy: {proxies}--------------------")
     results = engine.search(text, pages=2)
 
     info = ""
