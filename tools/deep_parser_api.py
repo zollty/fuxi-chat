020f280f25ce26594cf72e727a3bb320f4c19e10
zollty <zollty@163.com>
2024-10-20 17:48:50 +0800

    update

--- tools/deep_parser_api.py
+++ tools/deep_parser_api.py
@@ -10,6 +10,9 @@ sys.path.append(runtime_root_dir)
 from fastapi import FastAPI, Body, Request, File, Form, UploadFile
 from starlette.responses import RedirectResponse
 from fuxi.utils.api_base import (BaseResponse, ListResponse)
+from jian.tools.config import TEXT_SPLITTER_NAME, CHUNK_SIZE, OVERLAP_SIZE, ZH_TITLE_ENHANCE
+from langchain.docstore.document import Document
+from jian.tools.text_splitter_helper import do_split_docs
 
 
 async def document():
@@ -19,6 +22,18 @@ async def document():
 def mount_deep_parser_app_routes(app: FastAPI):
     from fuxi.utils.runtime_conf import get_temp_dir
     from jian.tools.file_upload_parse import parse_files_in_thread, FileLoadReq, parse_files_by_url_in_thread
+
+    async def text_split_req(
+            text: str = Form(..., description="文本"),
+            text_splitter_name: str = Form(TEXT_SPLITTER_NAME, description="分段函数"),
+            chunk_size: int = Form(CHUNK_SIZE, description="知识库中单段文本最大长度"),
+            chunk_overlap: int = Form(OVERLAP_SIZE, description="知识库中相邻文本重合长度")
+    ) -> BaseResponse:
+        docs = do_split_docs([Document(page_content=text, metadata={})],
+                             text_splitter_name=text_splitter_name, chunk_size=chunk_size,
+                             chunk_overlap=chunk_overlap)
+        return BaseResponse(code=200, msg="文件分段完成", data=
+        [{"page_content": x.page_content, "metadata": x.metadata, "type": x.type} for x in docs])
 
     async def load_file_by_form_req(
             file: UploadFile = File(..., description="浏览器标准Form二进制文件对象"),
@@ -73,9 +88,53 @@ def mount_deep_parser_app_routes(app: FastAPI):
                                 data={"content": documents[0].page_content, "metadata": documents[0].metadata})
         return BaseResponse(code=500, msg="解析文件失败")
 
+    async def load_file_and_split_by_form_req(
+            file: UploadFile = File(..., description="浏览器标准Form二进制文件对象"),
+            file_name: str = Form(None, description="名称"),
+            file_format: str = Form(None, description="后缀"),
+            text_splitter_name: str = Form(TEXT_SPLITTER_NAME, description="分段函数"),
+            chunk_size: int = Form(CHUNK_SIZE, description="知识库中单段文本最大长度"),
+            chunk_overlap: int = Form(OVERLAP_SIZE, description="知识库中相邻文本重合长度")
+    ) -> BaseResponse:
+        docs = []
+        res = await load_file_by_form_req(file, file_name, file_format)
+        if res.code == 200:
+            docs.append(Document(page_content=res.data.content, metadata=res.data.metadata))
+        else:
+            return res
+        docs = do_split_docs(docs,
+                             text_splitter_name=text_splitter_name, chunk_size=chunk_size,
+                             chunk_overlap=chunk_overlap)
+        return BaseResponse(code=200, msg="文件解析及分段完成", data=
+                            [{"page_content": x.page_content, "metadata": x.metadata, "type": x.type} for x in docs])
+
+    async def load_file_and_split_by_url_req(
+            file_url: str = Form(..., description="文件URL"),
+            file_name: str = Form(None, description="名称"),
+            file_format: str = Form(None, description="后缀"),
+            text_splitter_name: str = Form(TEXT_SPLITTER_NAME, description="分段函数"),
+            chunk_size: int = Form(CHUNK_SIZE, description="知识库中单段文本最大长度"),
+            chunk_overlap: int = Form(OVERLAP_SIZE, description="知识库中相邻文本重合长度")
+    ) -> BaseResponse:
+        docs = []
+        res = await load_file_by_url_req(file_url, file_name, file_format)
+        if res.code == 200:
+            docs.append(Document(page_content=res.data.content, metadata=res.data.metadata))
+        else:
+            return res
+        docs = do_split_docs(docs,
+                             text_splitter_name=text_splitter_name, chunk_size=chunk_size,
+                             chunk_overlap=chunk_overlap)
+        return BaseResponse(code=200, msg="文件解析及分段完成", data=
+                            [{"page_content": x.page_content, "metadata": x.metadata, "type": x.type} for x in docs])
+
     # app.get("/",
     #         response_model=BaseResponse,
     #         summary="swagger 文档")(document)
+    app.post("/doc/text-split",
+             tags=["DeepParser"],
+             summary="文档分段（TextSplitter）"
+             )(text_split_req)
 
     app.post("/doc/load-file-content-by-form",
              tags=["DeepParser"],
@@ -86,6 +145,16 @@ def mount_deep_parser_app_routes(app: FastAPI):
              tags=["DeepParser"],
              summary="文档解析（不分段）（根据文件URL）"
              )(load_file_by_url_req)
+
+    app.post("/doc/parse-and-split-file-by-form",
+             tags=["DeepParser"],
+             summary="文档解析分段（根据临时上传文件）"
+             )(load_file_and_split_by_form_req)
+
+    app.post("/doc/parse-and-split-file-by-url",
+             tags=["DeepParser"],
+             summary="文档解析分段（根据文件url下载）"
+             )(load_file_and_split_by_url_req)
 
 
 if __name__ == "__main__":
